10c901d43c9b0c99adb645c2497c6bdb45fad549
Imadtv85 <Imadtv800@outlook.com>
2024-11-24 18:02:46 +0100

    Update stream2.sh

--- stream2.sh
+++ stream2.sh
@@ -2,4 +2,4 @@
 ffmpeg -i "http://ugeen.live:8080/Ugeen_VIP7HfpAs/DqxizE/3224" \
        -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
        -c:a aac -b:a 128k -ac 2 \
-       -f flv "rtmps://live-api-s.facebook.com:443/rtmp/FB-122166458174260989-0-AbxTPNanU35-EVYZ"
+       -f flv "rtmps://live-api-s.facebook.com:443/rtmp/FB-122166464408260989-0-AbyI4Xt-4970ce9d"
