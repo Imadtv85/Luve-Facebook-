c8ae804ad15d1ec2141ad53e87b27af97d06c4d3
Imadtv85 <Imadtv800@outlook.com>
2024-11-24 17:57:53 +0100

    Update stream2.sh

--- stream2.sh
+++ stream2.sh
@@ -1,4 +1,5 @@
-ffmpeg -i "https://live4.beinconnect.us/YallaGoalApp/beINSports3.m3u8" \
+#!/bin/bash
+ffmpeg -i "http://ugeen.live:8080/Ugeen_VIP7HfpAs/DqxizE/3224" \
        -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
        -c:a aac -b:a 128k -ac 2 \
        -f flv "rtmps://live-api-s.facebook.com:443/rtmp/FB-122166458174260989-0-AbxTPNanU35-EVYZ"
